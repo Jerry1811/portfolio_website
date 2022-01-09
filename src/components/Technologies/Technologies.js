24cd0c0e55e5e3ff31ac7315be381e61b73f5f4a
Jerry1811 <58642285+Jerry1811@users.noreply.github.com>
2022-01-09 10:00:01 +0000

    technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,54 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiReact, DiZend, DiNodejs } from 'react-icons/di';
+import { Si1Password } from "react-icons/si";
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id='tech'>
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I have worked with a range of technologies in the web development world.
+      From Back-end to Testing
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size='5rem' />
+        <ListContainer>
+          <ListTitle>Front-end</ListTitle>
+          <ListParagraph>
+            Experience with
+            <br />
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiNodejs size='5rem' />
+        <ListContainer>
+          <ListTitle>Back-end</ListTitle>
+          <ListParagraph>
+            Experience with
+            <br />
+            Node.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <Si1Password size='5rem' />
+        <ListContainer>
+          <ListTitle>Testing</ListTitle>
+          <ListParagraph>
+            Experience with
+            <br />
+            Tools like Selenium and Cypress
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
