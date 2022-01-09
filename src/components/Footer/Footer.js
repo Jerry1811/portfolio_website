173b3ea7c24ddbc0c75cb340f52098572b3b7f1f
Jerry1811 <58642285+Jerry1811@users.noreply.github.com>
2022-01-09 11:01:11 +0000

    accomplishments and footer sections

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillFacebook, AiFillLinkedin } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href='tel:+233-541-914-386'>+233-541-914-386</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href='mailto:jerryhukpati@gmail.com'>jerryhukpati@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+        <SocialIcons href='https://github.com/Jerry1811'>
+       <AiFillGithub size='3rem' />
+     </SocialIcons>
+     <SocialIcons href='https://www.linkedin.com/in/jeremiah-hukpati/'>
+       <AiFillLinkedin size='3rem' />
+     </SocialIcons>
+     <SocialIcons href='https://github.com/Jerry1811'>
+       <AiFillFacebook size='3rem' />
+     </SocialIcons>
+     </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
